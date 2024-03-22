7ce8a706772b7e8d19692999bf341f7d7381624f
Maksim <raymax.spb@gmail.com>
2024-03-22 21:12:29 +0300

    Homework 12 (Объекты и классы)

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,16 @@ public class Main {
         System.out.println("author1.getFirsName() = " + author1.getFirsName());
         System.out.println("author1.getLastName() = " + author1.getLastName());
 
+        System.out.println("");
 
+        Book book2 = new Book("Гордость и предубеждение",1813 );
+        Author author2 = new Author("Джейн","Остин");
+        System.out.println("book1.getBookName() = " + book2.getBookName());
+        System.out.println("book1.getYearPublic() = " + book2.getYearPublic());
+        System.out.println("author1.getFirsName() = " + author2.getFirsName());
+        System.out.println("author1.getLastName() = " + author2.getLastName());
+
+        book2.setYearPublic(2024);
+        System.out.println("book2.getYearPublic() = " + book2.getYearPublic());
     }
 }
