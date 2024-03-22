47c97dec509a057596fb9a110bfe86dd42dc5751
Maksim <raymax.spb@gmail.com>
2024-03-22 21:03:43 +0300

    Homework 12 (Объекты и классы)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Book book1 = new Book("Песнь льда и огня",1996 );
+        Author author1 = new Author("Джордж","Мартин");
+        System.out.println("book1.getBookName() = " + book1.getBookName());
+        System.out.println("book1.getYearPublic() = " + book1.getYearPublic());
+        System.out.println("author1.getFirsName() = " + author1.getFirsName());
+        System.out.println("author1.getLastName() = " + author1.getLastName());
+
 
     }
 }
