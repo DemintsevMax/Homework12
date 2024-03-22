7c6270e1e56063b0e4d0755111d30ef95bddeac0
Maksim <raymax.spb@gmail.com>
2024-03-22 20:19:21 +0300

    Homework 12 (Объекты и классы)

---
+++ src/Author.java
@@ -0,0 +1,9 @@
+public class Author {
+    String firsName;
+    String lastName;
+
+    public Author (String firsName, String lastName){
+        this.firsName = firsName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    String bookName;
+    int yearPublic;
+
+    public Book(String bookName, Author, int yearPublic){
+        this.bookName = bookName;
+        this.yearPublic = yearPublic;
+    }
+}
