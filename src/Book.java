df6e6118ec8a83cf53cdea5ef2a59057e33fdb21
Maksim <raymax.spb@gmail.com>
2024-03-23 11:14:49 +0300

    Homework 12 (Объекты и классы)

--- src/Book.java
+++ src/Book.java
@@ -1,20 +1,24 @@
 public class Book {
     private String bookName;
     private int yearPublic;
+    private Author author;
 
-    public Book(String bookName, int yearPublic) {
+    public Book(String bookName, int yearPublic, Author author) {
         this.bookName = bookName;
         this.yearPublic = yearPublic;
+        this.author = author;
     }
 
     public String getBookName() {
-
         return this.bookName;
     }
 
     public int getYearPublic() {
-
         return this.yearPublic;
+    }
+
+    public Author getAuthor() {
+        return this.author;
     }
 
     public void setYearPublic(int yearPublic) {
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Песнь льда и огня", 1996);
         Author author1 = new Author("Джордж", "Мартин");
-        System.out.println("book1.getBookName() = " + book1.getBookName());
-        System.out.println("book1.getYearPublic() = " + book1.getYearPublic());
-        System.out.println("author1.getFirsName() = " + author1.getFirsName());
-        System.out.println("author1.getLastName() = " + author1.getLastName());
+        Book book1 = new Book("Песнь льда и огня", 1996,author1);
+        System.out.println("Название книги: " + book1.getBookName());
+        System.out.println("Год публикации: " + book1.getYearPublic());
+        System.out.println("Автор: " + book1.getAuthor());
 
         System.out.println("");
 
-        Book book2 = new Book("Гордость и предубеждение", 1813);
         Author author2 = new Author("Джейн", "Остин");
-        System.out.println("book1.getBookName() = " + book2.getBookName());
-        System.out.println("book1.getYearPublic() = " + book2.getYearPublic());
-        System.out.println("author1.getFirsName() = " + author2.getFirsName());
-        System.out.println("author1.getLastName() = " + author2.getLastName());
+        Book book2 = new Book("Гордость и предубеждение", 1813, author2);
+        System.out.println("Название книги: = " + book2.getBookName());
+        System.out.println("Год публикации: " + book2.getYearPublic());
+        System.out.println("Автор: " + book2.getAuthor());
 
         book2.setYearPublic(2024);
-        System.out.println("book2.getYearPublic() = " + book2.getYearPublic());
+        System.out.println("Год публикации: = " + book2.getYearPublic());
     }
 }
