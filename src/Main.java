dd90b5b217ab7ea7df69578f7454de31412059bc
Maksim <raymax.spb@gmail.com>
2024-03-23 11:34:32 +0300

    Homework 12 (Объекты и классы)

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Book book1 = new Book("Песнь льда и огня", 1996,author1);
         System.out.println("Название книги: " + book1.getBookName());
         System.out.println("Год публикации: " + book1.getYearPublic());
-        System.out.println("Автор: " + book1.getAuthor());
+        System.out.println("Автор: " + book1.getAuthor().getFirsName() + " " + book1.getAuthor().getLastName());
 
         System.out.println("");
 
@@ -12,7 +12,7 @@ public class Main {
         Book book2 = new Book("Гордость и предубеждение", 1813, author2);
         System.out.println("Название книги: = " + book2.getBookName());
         System.out.println("Год публикации: " + book2.getYearPublic());
-        System.out.println("Автор: " + book2.getAuthor());
+        System.out.println("Автор: " + book2.getAuthor().getFirsName() + " " + book2.getAuthor().getLastName());
 
         book2.setYearPublic(2024);
         System.out.println("Год публикации: = " + book2.getYearPublic());
